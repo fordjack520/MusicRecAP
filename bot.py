d5d823329385786fe57068e42a7f6bd72501cd73
fordjack520 <fordjack520@yahoo.com>
2023-10-20 02:48:16 -0700

    Added duration functionality

--- bot.py
+++ bot.py
@@ -12,14 +12,16 @@ rec_url = re.search("(?P<url>https?://[^\s]+)", inp).group("url")
 
 return_url = requests.get(rec_url).url
 
-print(return_url)
+# print(return_url)
 
 if "album" in return_url:
     album = sp.album(return_url)
+    item_type = album["album_type"]
 elif "track" in return_url:
     track = sp.track(return_url)
     track_name = track["name"]
     album = track["album"]
+    item_type = track["type"]
     print(f"This song is \"{track_name}\"")
 else:
     print("Invalid input.")
@@ -27,4 +29,18 @@ else:
 album_name = album["name"]
 album_artist = album["artists"][0]["name"]
 album_yr = album["release_date"][0:4]
-print (f"This album is {album_name} ({album_yr}) by {album_artist}")
+album_tracks = album["total_tracks"]
+
+if item_type == "track":
+    duration = track["duration_ms"]
+else:
+    duration = 0
+    for item in album["tracks"]["items"]:
+        duration += item["duration_ms"]
+
+duration_sec = divmod(duration, 1000)[0]
+duration_min, duration_sec = divmod(duration_sec, 60)
+duration_time = f"{duration_min}:{duration_sec}"
+
+print (f"This album is {album_name} ({album_yr}) by {album_artist}. It contains {album_tracks} tracks and runs for {duration_time}.")
+print (f"This is a(n) {item_type}.")
