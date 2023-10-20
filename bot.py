b12fd9ae7f94b67cd46ca3230884785ef3ba2dcf
fordjack520 <fordjack520@yahoo.com>
2023-10-20 16:47:32 -0700

    Converted variables to keys in dict "item"

--- bot.py
+++ bot.py
@@ -12,35 +12,39 @@ rec_url = re.search("(?P<url>https?://[^\s]+)", inp).group("url")
 
 return_url = requests.get(rec_url).url
 
-# print(return_url)
+item = {}
+
+print(return_url)
 
 if "album" in return_url:
     album = sp.album(return_url)
-    item_type = album["album_type"]
+    item["type"] = album["album_type"]
 elif "track" in return_url:
     track = sp.track(return_url)
-    track_name = track["name"]
+    item["track_name"] = track["name"]
     album = track["album"]
-    item_type = track["type"]
-    print(f"This song is \"{track_name}\"")
+    item["type"] = track["type"]
+    print(f"This song is \"{item['track_name']}\"")
 else:
     print("Invalid input.")
 
-album_name = album["name"]
-album_artist = album["artists"][0]["name"]
-album_yr = album["release_date"][0:4]
-album_tracks = album["total_tracks"]
+item["album_name"] = album["name"]
+item["album_artist"] = album["artists"][0]["name"]
+item["year"] = album["release_date"][0:4]
+item["number_of_tracks"] = album["total_tracks"]
 
-if item_type == "track":
+if item["type"] == "track":
     duration = track["duration_ms"]
 else:
     duration = 0
-    for item in album["tracks"]["items"]:
-        duration += item["duration_ms"]
+    for t in album["tracks"]["items"]:
+        duration += t["duration_ms"]
 
 duration_sec = divmod(duration, 1000)[0]
 duration_min, duration_sec = divmod(duration_sec, 60)
-duration_time = f"{duration_min}:{duration_sec}"
+item["duration"] = f"{duration_min}:{duration_sec}"
 
-print (f"This album is {album_name} ({album_yr}) by {album_artist}. It contains {album_tracks} tracks and runs for {duration_time}.")
-print (f"This is a(n) {item_type}.")
+print (f'This album is {item["album_name"]} ({item["year"]}) by {item["album_artist"]}. It contains {item["number_of_tracks"]} tracks and runs for {item["duration"]}.')
+print (f'This is a(n) {item["type"]}.')
+
+#print(item)
