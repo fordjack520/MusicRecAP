6d316aa1067b9f397861ab17178d81631e13a028
fordjack520 <fordjack520@yahoo.com>
2023-10-20 21:26:48 -0700

    Added ~full_sweep command

--- bot.py
+++ bot.py
@@ -1,17 +1,20 @@
 import discord
 import responses
 import creds
+from discord.ext import commands
 
-async def send_message(message, user_message):
+async def send_message(message, user_message, quiet):
     try:
-        response = responses.handle_response(user_message)
-        await message.channel.send(response)
+        response = responses.handle_response(user_message, quiet)
+        if quiet == False:
+            await message.channel.send(response)
+
     except Exception as exc:
         print(exc)
 
 def run_bot():
     TOKEN = creds.discord_bot_token
-    client = discord.Client()
+    client = commands.Bot(command_prefix="~")
 
     @client.event
     async def on_ready():
@@ -19,6 +22,7 @@ def run_bot():
     
     @client.event
     async def on_message(message):
+        await client.process_commands(message)
         if message.author == client.user:
             return
         
@@ -26,7 +30,7 @@ def run_bot():
         user_message= str(message.content)
         channel = str(message.channel)
 
-        if channel != "bot-allow":
+        if channel != "music-recs":
             return
 
         if "spotify" and "https:" not in user_message:
@@ -34,6 +38,17 @@ def run_bot():
 
         print(f'{username} said: {user_message}')
 
-        await send_message(message, user_message)
+        await send_message(message, user_message, quiet=False)
+    
+    @client.command()
+    async def full_sweep(ctx):
+        if ctx.channel != "music-recs":
+            return
+        counter = 0
+        async for message in ctx.channel.history(limit=500):
+            if "spotify" in str(message.content):
+                await send_message(message,str(message.content),quiet=True)
+                counter += 1
+        await ctx.channel.send(f"{counter} items have been successfully added to the spreadsheet! {ctx.channel}")
     
     client.run(TOKEN)
